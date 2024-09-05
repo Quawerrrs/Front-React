55655edfcffddad32ca08180dff1c0ebe5ef5d2f
Amurius <artuslr78@gmail.com>
2024-09-05 14:22:38 +0200

    securePagesEntCrea

--- front/src/Entreprise/Entreprise.jsx
+++ front/src/Entreprise/Entreprise.jsx
@@ -1,7 +1,60 @@
-export default function  Entreprise(){
+import { useState, useEffect } from "react";
+
+export default function Entreprise() {
+  const [validEntreprise, setValidEntreprise] = useState(false);
+  const [start, setStart] = useState(0);
+  const [length, setLength] = useState(5);
+  const [chaines, setChaines] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/session/getSession", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((token) => {
+        if (token.siren !== undefined) {
+          setValidEntreprise(true);
+        } else {
+          setValidEntreprise(false);
+        }
+      });
+
+    getMoreChannels();
+  }, []);
+
+  const getMoreChannels = () => {
+    console.log("BITE");
+
+    fetch("http://localhost:5000/api/getchannels", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ start: start, length: length }),
+    })
+      .then((response) => response.json())
+      .then((channels) => {
+        setChaines((prevChaines) => [...prevChaines, ...channels]);
+        setStart(start + length);
+        console.log(channels);
+      });
+  };
+
+  if (validEntreprise && chaines) {
     return (
-        <div>
-            C'est ici les entreprises
-        </div>
+      <div>
+        {chaines.map((channel, index) => (
+          <div key={index}>
+            <p>
+              {channel.cha_name} a {channel.cha_subs} abonnés et porte sur le thème :{" "}
+              {channel.cha_theme_1}
+            </p>
+          </div>
+        ))}
+        <button onClick={getMoreChannels}>Load More</button>
+      </div>
     );
+  } else {
+    return null;
+  }
 }
