6c3fed53912dc6770e19b7b9d989044e606e70da
Amurius <artuslr78@gmail.com>
2024-09-19 15:10:41 +0200

    protectionroutes

--- front/src/Createur/Createur.jsx
+++ front/src/Createur/Createur.jsx
@@ -28,25 +28,27 @@ export default function Createur() {
         setChaines(chaines);
       });
   }, []);
-  return (
-    <>
-      <nav className="bg-gray-800 p-4 flex justify-between items-center">
-        <div className="text-orange-500 text-xl font-bold">Streamio</div>
-        <div className="space-x-4">
-          <a
-            href="/"
-            className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline"
-          >
-            Accueil
-          </a>
-          <a
-            href="/Profile"
-            className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline"
-          >
-            Profil
-          </a>
-        </div>
-      </nav>
-    </>
-  );
+  if (validCreateur) {
+    return (
+      <>
+        <nav className="bg-gray-800 p-4 flex justify-between items-center">
+          <div className="text-orange-500 text-xl font-bold">Streamio</div>
+          <div className="space-x-4">
+            <a
+              href="/"
+              className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline"
+            >
+              Accueil
+            </a>
+            <a
+              href="/Profile"
+              className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700 no-underline"
+            >
+              Profil
+            </a>
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
